07f529e5588685a4bd6e2c0d19579d6bc569b71e
Ramiro Adrover <72950206+RamiroAdrover@users.noreply.github.com>
2023-09-29 13:16:28 -0300

    Update index.blade.php
    Correcciones de estilos

--- resources/views/plan/index.blade.php
+++ resources/views/plan/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-700 leading-tight">
             {{ __('Planes') }}
         </h2>
     </x-slot>
@@ -23,34 +23,59 @@
                 <!-- Lista de elementos -->
                 <ul id="listaPlanes" class="list-group">
                     <!-- Elemento 1 con margen superior e inferior -->
-                    <li class="list-group-item flex justify-between items-center mt-2 mb-2">
-                        <span class="flex-grow font-semibold">Nombre del Plan 1</span>
-                        <span>
-                            <button class="btn btn-primary border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Modificar</button>
-                            <button class="btn btn-danger border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Dar de Baja</button>
-                        </span>
+                    <li class="custom-list-item mt-2 mb-2">
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <span class="font-semibold">Nombre del Plan 1</span>
+                                <!-- Subítemes dentro del primer elemento -->
+                                <ul class="custom-sub-list ml-8">
+                                    <li>Prestación 1</li>
+                                    <li>Prestación 2</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Modificar</button>
+                                <button class="btn btn-danger border border-gray-700 rounded-full px-4 py-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Dar de Baja</button>
+                            </div>
+                        </div>
                     </li>
-                    <!-- Subítemes dentro del primer elemento -->
-                    <ul class="ml-8">
-                        <li>Prestación 1</li>
-                        <li>Prestación 2</li>
-                    </ul>
 
                     <!-- Elemento 2 con margen superior e inferior -->
-                    <li class="list-group-item flex justify-between items-center mt-2 mb-2">
-                        <span class="flex-grow font-semibold">Nombre del Plan 2</span>
-                        <span>
-                            <button class="btn btn-primary border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Modificar</button>
-                            <button class="btn btn-danger border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Dar de Baja</button>
-                        </span>
+                    <li class="custom-list-item mt-2 mb-2">
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <span class="font-semibold">Nombre del Plan 2</span>
+                                <!-- Subítemes dentro del segundo elemento -->
+                                <ul class="custom-sub-list ml-8">
+                                    <li>Prestación 3</li>
+                                    <li>Prestación 4</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary border border-gray-700 rounded-full px-4 py-2 mr-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Modificar</button>
+                                <button class="btn btn-danger border border-gray-700 rounded-full px-4 py-2 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Dar de Baja</button>
+                            </div>
+                        </div>
                     </li>
-                    <!-- Subítemes dentro del segundo elemento -->
-                    <ul class="ml-8">
-                        <li>Prestación 3</li>
-                        <li>Prestación 4</li>
-                    </ul>
                 </ul>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<style>
+    /* Estilo personalizado para los elementos de la lista */
+    .custom-list-item {
+        background-color: #f3f4f6; /* Cambia el fondo a un tono más gris */
+        border-radius: 8px; /* Redondea los bordes */
+        padding: 8px; /* Añade espaciado interno */
+    }
+
+    /* Estilo personalizado para los subítemes de la lista */
+    .custom-sub-list {
+        background-color: #f3f4f6; /* Cambia el fondo a un tono más gris */
+        border-radius: 8px; /* Redondea los bordes */
+        padding: 4px; /* Añade espaciado interno */
+        list-style-type: none; /* Elimina las viñetas de la lista */
+    }
+</style>
