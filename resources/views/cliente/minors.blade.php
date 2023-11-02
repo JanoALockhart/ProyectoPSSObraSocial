e0e09e1f55ea3137cefddf7af4fc5fc0e9748eca
JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>
2023-11-02 20:50:05 -0300

    Se agrego mensaje
    Cuando el cliente no tiene menores asociados se muestra el mensaje "Usted no tiene menores de edad asociados"

--- resources/views/cliente/minors.blade.php
+++ resources/views/cliente/minors.blade.php
@@ -5,7 +5,7 @@
     <div class="">
         <h3 class="m-3 text-center text-3xl font-bold dark:text-white">Menores Registrados</h3>
             <div class="mx-auto max-h-80 max-w-5xl p-3 overflow-y-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                @foreach ($clientMinors as $minor)
+                @forelse ($clientMinors as $minor)
                     <div class="flex p-3 my-3 bg-blue-200 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <div class="grow flex items-center justify-center">
                             <p class="m-1 text-lg text-gray-900 dark:text-white">DNI: {{ $minor->DNI }} </p>
@@ -13,7 +13,9 @@
                             <p class="m-1 text-lg text-gray-900 dark:text-white">Plan Asociado: {{ $minor->dni }} </p>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <h3>Usted no tiene menores de edad asociados. </h3>
+                @endforelse
             </div>
     </div>
 
